d0667098a39b93a9d26c47cf73fe3a3ed4c75a33
Brandon Fearing <brandon.fearing@gmail.com>
2023-08-06 12:39:17 -0400

    more mobile tweaks for login

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -11,7 +11,7 @@ const AuthLayout = ({ meta, children }: AuthLayoutProps) => {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <Metadata meta={meta} />
-      <main className="container relative grid flex-col items-center justify-center h-screen lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <main className="container relative grid flex-col items-center justify-center h-full bg-cover lg:max-w-none lg:grid-cols-2 lg:px-0 bg-space lg:bg-none">
         {children}
       </main>
       <TailwindIndicator />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,7 +45,7 @@ export default function AuthenticationPage() {
           </blockquote>
         </div>
       </div>
-      <div className="lg:p-8">
+      <div className="p-10 lg:p-8 bg-background rounded-2xl">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
