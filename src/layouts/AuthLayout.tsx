35268e71b2a6c7a57b33b79a9e30cafa707a9e18
Brandon Fearing <brandon.fearing@gmail.com>
2023-08-06 13:05:49 -0400

    layout adj

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -11,7 +11,7 @@ const AuthLayout = ({ meta, children }: AuthLayoutProps) => {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <Metadata meta={meta} />
-      <main className="m-2.5 sm:m-5 relative grid flex-col items-center justify-center bg-cover h-screen max-h-[-webkit-fill-available] lg:max-w-none lg:grid-cols-2 lg:px-0 bg-space lg:bg-none">
+      <main className="relative grid flex-col items-center justify-center bg-cover h-screen max-h-[-webkit-fill-available] lg:max-w-none lg:grid-cols-2 lg:px-0 bg-space lg:bg-none">
         {children}
       </main>
       <TailwindIndicator />
